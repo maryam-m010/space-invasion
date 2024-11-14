1fe1f45779262b10352dfc37327221df8cb6e34c
maryam-m010 <mmaryam7@outlook.com>
2024-11-14 18:29:08 +0000

    Update space_invasion.py

--- space_invasion.py
+++ space_invasion.py
@@ -39,29 +39,70 @@ def draw(yellow, red, red_health, yellow_health):
 
     pygame.display.update()
 
+#yellow key movements
+
+#left
 def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a]:
+    if keys_pressed[pygame.K_a] and yellow.x > 0:
         yellow.x -= VEL
 
+#right
+    if keys_pressed[pygame.K_d] and yellow.x + yellow.width < border.x:
+        yellow.x += VEL
+
+#up
+    if keys_pressed[pygame.K_w] and yellow.y > 0:
+        yellow.y -= VEL
+
+#down
+    if keys_pressed[pygame.K_s] and yellow.y + yellow.height < HEIGHT:
+        yellow.y += VEL
+
+#red key movements
+
+#left
 def red_handle_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_LEFT]:
+    if keys_pressed[pygame.K_LEFT] and red.x > border.x + border.width:
         red.x -= VEL
 
-#right = plus, up = plus, down = minus
+#right
+    if keys_pressed[pygame.K_RIGHT] and red.x + red.width < WIDTH:
+        red.x += VEL
+
+#up
+    if keys_pressed[pygame.K_UP] and red.y > 0:
+        red.y -= VEL
+
+#down
+    if keys_pressed[pygame.K_DOWN]and red.y + red.height < HEIGHT:
+        red.y += VEL
+
+def handle_bullets():
+    pass
+
+yellow = pygame.Rect(100, 300, sw, sh)
+red = pygame.Rect(700, 300, sw, sh)
+
+red_bullets = []
+yellow_bullets= []
+
+max_bullet = 3
 
 while True:
-
-    yellow = pygame.Rect(100, 300, sw, sh)
-    red = pygame.Rect(700, 300, sw, sh)
 
     red_health = 10
     yellow_health = 10
 
     keys_pressed = pygame.key.get_pressed()
     yellow_handle_movement(keys_pressed, yellow)
+    red_handle_movement(keys_pressed, red)
 
     draw(yellow, red, red_health, yellow_health)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LCTRL and len(yellow_bullets) < max_bullet:
+                bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.y // 2, 10, 5)
+                yellow_bullets.append(bullet)
